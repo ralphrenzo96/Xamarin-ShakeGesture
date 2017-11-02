c220270eda92608bf46d636cf9ce48ac2dacc124
Ralph Renzo Pitogo <ralph.pitogo@appstone.tech>
2017-11-02 11:00:02 +0800

    Removed status bar on Android and iOS

---
+++ Droid/Helpers/StatusBarService/StatusBarService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Views;
+using cprapp.Droid.Helpers.StatusBarService;
+using cprapp.Helpers.StatusBarService;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(StatusBarService))]
+namespace cprapp.Droid.Helpers.StatusBarService
+{
+    public class StatusBarService : IStatusBarService
+    {
+		WindowManagerFlags _originalFlags;
+
+		#region IStatusBar implementation
+
+		public void HideStatusBar()
+		{
+			var activity = (Activity)Forms.Context;
+			var attrs = activity.Window.Attributes;
+			_originalFlags = attrs.Flags;
+			attrs.Flags |= Android.Views.WindowManagerFlags.Fullscreen;
+			activity.Window.Attributes = attrs;
+		}
+
+		public void ShowStatusBar()
+		{
+			var activity = (Activity)Forms.Context;
+			var attrs = activity.Window.Attributes;
+			attrs.Flags = _originalFlags;
+			activity.Window.Attributes = attrs;
+		}
+
+		#endregion
+	}
+}
---
+++ iOS/Helpers/StatusBarService/StatusBarService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using cprapp.iOS.Helpers.StatusBarService;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(StatusBarService))]
+namespace cprapp.iOS.Helpers.StatusBarService
+{
+    public class StatusBarService
+    {
+		#region IStatusBar implementation
+
+		public void HideStatusBar()
+		{
+			UIApplication.SharedApplication.StatusBarHidden = true;
+		}
+
+		public void ShowStatusBar()
+		{
+			UIApplication.SharedApplication.StatusBarHidden = false;
+		}
+
+		#endregion
+	}
+}
